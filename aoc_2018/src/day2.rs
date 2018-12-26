1b113efd109314cd1b4e4c08cd588372252c261f
Tunahan Karlıbaş <dvt.tnhn.krlbs@gmail.com>
2018-12-26 13:56:23 +0300

    Add github.com/Erk- s solution for benchmark purposes (it runs fast)

--- aoc_2018/src/day2.rs
+++ aoc_2018/src/day2.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, BTreeMap};
+use std::fmt::Write;
 
 #[aoc(day2, part1, HashMap)]
 pub fn day2_part1_solve_hash(input: &str) -> i32 {
@@ -75,4 +76,82 @@ pub fn day2_part2_naive(input: &str) -> String {
         }
     }
     result
+}
+
+// github.com/Erk- s solution 
+#[aoc(day2, part2, Erk)]
+fn part2(input: &str) -> String {
+    let mut firstmap: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut secondmap: HashMap<&str, Vec<&str>> = HashMap::new();
+    let mut input_iter = input.split_whitespace();
+    let fst = input_iter.next().unwrap();
+    let splt = fst.len()/2;
+    {
+        let (a, b) = fst.split_at(splt);
+        firstmap.insert(a, vec![fst]);
+        secondmap.insert(b, vec![fst]);
+    }
+    for e in input_iter {
+        let (a, b) = e.split_at(splt);
+        firstmap.entry(a).and_modify(|v| v.push(e)).or_insert_with(|| vec![e]);
+        secondmap.entry(b).and_modify(|v| v.push(e)).or_insert_with(|| vec![e]);
+    }
+    // https://users.rust-lang.org/t/composing-a-list-of-all-pairs/15475/3
+    for e in firstmap.values() {
+        if e.len() > 1 {
+            //println!("{:?}", e);
+            let pairs: Vec<(&str, &str)> = e.iter()
+                .enumerate()
+                .flat_map(move |t| std::iter::repeat(t.1).zip(e.iter().skip(t.0 + 1)))
+                .map(|(a,b)| (*a, *b))
+                .collect();
+            for p in pairs {
+                if is_one_off(p) {
+                    return print_diff(p);
+                }
+            }
+        }
+    }
+    for e in secondmap.values() {
+        if e.len() > 1 {
+            //println!("{:?}", e);
+            let pairs: Vec<(&str, &str)> = e.iter()
+                .enumerate()
+                .flat_map(move |t| std::iter::repeat(t.1).zip(e.iter().skip(t.0 + 1)))
+                .map(|(a,b)| (*a, *b))
+                .collect();
+            for p in pairs {
+                if is_one_off(p) {
+                    return print_diff(p);
+                }
+            }
+        }
+    }
+    panic!("NONE FOUND!");
+}
+
+fn is_one_off(input: (&str, &str)) -> bool {
+    let iter = input.0.chars().zip(input.1.chars());
+    let mut one_diff = false;
+    for e in iter {
+        if e.0 != e.1 {
+            if one_diff {
+                return false;
+            } else {
+                one_diff = true;
+            }
+        }
+    }
+    true
+}
+
+fn print_diff(input: (&str, &str)) -> String {
+    let mut ret_str = String::new();
+    let iter = input.0.chars().zip(input.1.chars());
+    for e in iter {
+        if e.0 == e.1 {
+            let _ = write!(ret_str, "{}",  e.0);
+        }
+    }
+    ret_str
 }
