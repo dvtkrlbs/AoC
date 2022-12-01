798032e33e219d622087afecee9ee43fbe3d332d
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2022-12-01 22:25:51 +0200

    get rid of itertools use only a fold

--- aoc_2022/src/day1.rs
+++ aoc_2022/src/day1.rs
@@ -10,35 +10,40 @@ fn atoi(bytes: &[u8]) -> i64 {
 
 #[aoc(day1, part1)]
 pub fn day1_part1(input: &[u8]) -> i64 {
-    input
-        .split(|b| *b == b'\n')
-        .group_by(|line| *line == b"")
-        .into_iter()
-        .filter(|(key, _)| !key)
-        .map(|(_, group)| group.map(|val| atoi(val)).sum())
-        .max()
-        .unwrap()
+    let res = input.split(|b| *b == b'\n').fold((0, 0), |acc, x| {
+        if x == b"" {
+            if acc.1 > acc.0 {
+                (acc.1, 0)
+            } else {
+                (acc.0, 0)
+            }
+        } else {
+            let x = atoi(x) + acc.1;
+            (acc.0, x)
+        }
+    });
+
+    res.0
 }
 
 #[aoc(day1, part2)]
 pub fn day1_part2(input: &[u8]) -> i64 {
-    let res = input
-        .split(|b| *b == b'\n')
-        .group_by(|line| *line == b"")
-        .into_iter()
-        .filter(|(key, _)| !key)
-        .map(|(_, group)| group.map(|val| atoi(val)).sum::<i64>())
-        .fold((0, 0, 0), |acc, x| {
+    let res = input.split(|b| *b == b'\n').fold((0, 0, 0, 0), |acc, x| {
+        if x == b"" {
+            let x = atoi(x) + acc.3;
             if x > acc.0 {
-                (x, acc.0, acc.1)
+                (x, acc.0, acc.1, 0)
             } else if x > acc.1 {
-                (acc.0, x, acc.1)
+                (acc.0, x, acc.1, 0)
             } else if x > acc.2 {
-                (acc.0, acc.1, x)
+                (acc.0, acc.1, x, 0)
             } else {
-                acc
+                (acc.0, acc.1, acc.2, 0)
             }
-        });
+        } else {
+            (acc.0, acc.1, acc.2, acc.3 + atoi(x))
+        }
+    });
 
     res.0 + res.1 + res.2
 }
