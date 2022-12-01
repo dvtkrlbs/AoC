6d48fa92eb31d287b5e647e09aa13f8396ea1b9e
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2022-12-01 23:25:50 +0200

    use a mutable acclumator along with inline atoi

--- aoc_2022/src/day1.rs
+++ aoc_2022/src/day1.rs
@@ -1,5 +1,3 @@
-use itertools::Itertools;
-
 fn atoi(bytes: &[u8]) -> i64 {
     let mut result = 0;
     for byte in bytes {
@@ -26,6 +24,29 @@ pub fn day1_part1(input: &[u8]) -> i64 {
     res.0
 }
 
+#[aoc(day1, part1, Horrible)]
+pub fn day1_part1_horrible(input: &[u8]) -> i64 {
+    let res = input.iter().fold((0, 0, 0), |mut acc, x| match x {
+        b'\n' => {
+            if acc.2 == 0 {
+                acc.0 = acc.0.max(acc.1);
+                acc.1 = 0;
+                acc.2 = 0;
+            } else {
+                acc.1 += acc.2;
+                acc.2 = 0;
+            }
+            acc
+        }
+        &x => {
+            acc.2 = acc.2 * 10 + x.wrapping_sub(b'0') as i64;
+            acc
+        }
+    });
+
+    res.0
+}
+
 #[aoc(day1, part2)]
 pub fn day1_part2(input: &[u8]) -> i64 {
     let res = input.split(|b| *b == b'\n').fold((0, 0, 0, 0), |acc, x| {
@@ -42,6 +63,39 @@ pub fn day1_part2(input: &[u8]) -> i64 {
             }
         } else {
             (acc.0, acc.1, acc.2, acc.3 + atoi(x))
+        }
+    });
+
+    res.0 + res.1 + res.2
+}
+
+#[aoc(day1, part2, Horrible)]
+pub fn day1_part2_horrible(input: &[u8]) -> i64 {
+    let res = input.iter().fold((0, 0, 0, 0, 0), |mut acc, x| match x {
+        b'\n' => {
+            if acc.4 == 0 {
+                let x = acc.3;
+                if x > acc.0 {
+                    acc.2 = acc.1;
+                    acc.1 = acc.0;
+                    acc.0 = x;
+                } else if x > acc.1 {
+                    acc.2 = acc.1;
+                    acc.1 = x;
+                } else if x > acc.2 {
+                    acc.2 = x;
+                }
+                acc.3 = 0;
+                acc.4 = 0;
+            } else {
+                acc.3 += acc.4;
+                acc.4 = 0;
+            }
+            acc
+        }
+        x => {
+            acc.4 = acc.4 * 10 + x.wrapping_sub(b'0') as i64;
+            acc
         }
     });
 
