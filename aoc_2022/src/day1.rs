9e2f00da0e6c2990e45537985a1042dbe0ba02f0
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2022-12-01 21:38:39 +0200

    fold based max handling

--- aoc_2022/src/day1.rs
+++ aoc_2022/src/day1.rs
@@ -22,13 +22,23 @@ pub fn day1_part1(input: &[u8]) -> i64 {
 
 #[aoc(day1, part2)]
 pub fn day1_part2(input: &[u8]) -> i64 {
-    input
+    let res = input
         .split(|b| *b == b'\n')
         .group_by(|line| *line == b"")
         .into_iter()
         .filter(|(key, _)| !key)
         .map(|(_, group)| group.map(|val| atoi(val)).sum::<i64>())
-        .sorted_by(|a, b| Ord::cmp(&b, &a))
-        .take(3)
-        .sum::<i64>()
+        .fold((0, 0, 0), |acc, x| {
+            if x > acc.0 {
+                (x, acc.0, acc.1)
+            } else if x > acc.1 {
+                (acc.0, x, acc.1)
+            } else if x > acc.2 {
+                (acc.0, acc.1, x)
+            } else {
+                acc
+            }
+        });
+
+    res.0 + res.1 + res.2
 }
