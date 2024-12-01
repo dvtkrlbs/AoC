45f9b09e32c629dd12f86ff286a7f04eccae0da0
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2024-12-02 01:01:25 +0300

    cleanup

--- aoc_2024/src/day1.rs
+++ aoc_2024/src/day1.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, hash::Hash};
+#![allow(dead_code)]
+use std::collections::HashMap;
 
 use aoc_runner_derive::aoc;
 
@@ -42,39 +43,35 @@ fn part2(input: &str) -> u32 {
     similarity
 }
 
-// Hypex solution seems faster than mine but I don't think the counting solution for the right side is optimal
-// #[aoc(day1, part2)]
-// fn alternate_part2(input: &str) -> i32 {
-//     let (left, right): (Vec<i32>, Vec<i32>) = input
-//         .lines()
-//         .map(|line| line.split_once("   ").unwrap())
-//         .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
-//         .unzip();
+// Hypex solution
+fn alternate_part2(input: &str) -> i32 {
+    let (left, right): (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
+        .unzip();
 
-//     let mut score: i32 = 0;
-//     for id in left {
-//         score += id * right.iter().filter(|n| n == &&id).count() as i32
-//     }
-//     score
-// }
+    let mut score: i32 = 0;
+    for id in left {
+        score += id * right.iter().filter(|n| n == &&id).count() as i32
+    }
+    score
+}
 
-// #[aoc(day1, part2)]
-// fn alternate_part2(input: &str) -> i32 {
-//     let mut hashmap = HashMap::new();
-//     let left: Vec<i32> = input
-//         .lines()
-//         .map(|line| line.split_once("   ").unwrap())
-//         .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
-//         .map(|(a, b)| {
-//             hashmap.entry(b).and_modify(|v| *v += 1).or_insert(1);
+// Optimized solution of hypex with hashmap to have better complexity in the calculation
+fn alternate_part2_optimized(input: &str) -> i32 {
+    let mut hashmap = HashMap::new();
+    let left: Vec<i32> = input
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(a, b)| (a.parse::<i32>().unwrap(), b.parse::<i32>().unwrap()))
+        .map(|(a, b)| {
+            hashmap.entry(b).and_modify(|v| *v += 1).or_insert(1);
 
-//             a
-//         })
-//         .collect();
+            a
+        })
+        .collect();
 
-//     let mut score: i32 = 0;
-//     for id in left {
-//         score += id * hashmap.get(&id).unwrap_or(&0);
-//     }
-//     score
-// }
+    left.iter()
+        .fold(0, |acc, key| acc + (key * hashmap.get(key).unwrap_or(&0)))
+}
